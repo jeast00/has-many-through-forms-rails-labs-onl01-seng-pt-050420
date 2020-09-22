9819d80ce2971b90027182918809ab535e5ea0e7
Jonathan East <jonathan_east@ymail.com>
2020-09-22 18:26:26 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   def user_attributes=(user_attributes)
-    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes([:username]).blank?
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
   end
 end
